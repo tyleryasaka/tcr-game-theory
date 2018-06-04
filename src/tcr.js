b8e5473b64a25f0702317e737b39b7b04dcc8437
Tyler Yasaka <contact@tyleryasaka.me>
2018-06-03 23:41:40 -0700

    tcr payoff implementation
    This now has fully functional methods for hypothetical getting payoffs for all players involved, one payoff for each action - based on the actions of the other players.
    
    We'll be able to use this to determine the best strategy for each player in a given scenario, which we can use to determine whether a given scenario is in a state of equilibrium.

--- src/tcr.js
+++ src/tcr.js
@@ -1,28 +1,45 @@
+const actionList = 'list'
+const actionNotList = 'not_list'
+
+const actionChallenge = 'challenge'
+const actionNotChallenge = 'not_challenge'
+
+const actionAccept = 'accept'
+const actionReject = 'reject'
+
+const voteAccept = 'accept'
+const voteReject = 'reject'
+
+class Payoff {
+  constructor({ action, value } = {}) {
+    this.action = action
+    this.value = value
+  }
+}
+
 class Player {
-  constructor({ tokens = 0, registryValue = 0, quality = 0, isListed = false } = {}) {
+  constructor({ tokens = 0, registryValue = 0, quality = 0, isListed = false, vote } = {}) {
     this.tokens = tokens
     this.registryValue = registryValue
     this.quality = quality
     this.isListed = isListed
-  }
-}
-
-class Action {
-  constructor({ action, payoff } = {}) {
-    this.action = action
-    this.payoff = payoff
+    this.vote = vote
   }
 }
 
 class TCR {
   constructor({
     players = [],
+    candidate,
+    challenger,
     minDeposit,
     dispensationPct,
     minorityBlocSlash,
     voteQuorum
   } = {}) {
     this.players = players
+    this.candidate = candidate
+    this.challenger = challenger
     this.registry = []
     this.minDeposit = minDeposit
     this.dispensationPct = dispensationPct
@@ -30,65 +47,144 @@ class TCR {
     this.voteQuorum = voteQuorum
   }
 
-  candidates() {
-    return this.players.filter(({ registryValue }) => {
-      return registryValue > 0
+  getCandidatePayoff() {
+    const columnAccept = 'accept'
+    const columnReject = 'reject'
+    const columnNotChallenge = 'not_challenge'
+
+    const candidate = this.getPlayers()[this.candidate]
+    const isChallenged = this.getIsChallenged()
+    const challengeResult = getVerdict(this.getPlayers(), this.voteQuorum) ? columnAccept : columnReject
+    const columnSelected = !isChallenged ? columnNotChallenge : challengeResult
+
+    let matrix = {}
+    matrix[actionList] = {}
+    matrix[actionNotList] = {}
+
+    matrix[actionList][columnAccept] = this.minDeposit * this.dispensationPct
+    matrix[actionList][columnReject] = -1 * this.minDeposit
+    matrix[actionList][columnNotChallenge] = 0
+
+    matrix[actionNotList][columnAccept] = 0
+    matrix[actionNotList][columnReject] = 0
+    matrix[actionNotList][columnNotChallenge] = 0
+
+    return [
+      new Payoff({ action: actionList, value: matrix[actionList][columnSelected] }),
+      new Payoff({ action: actionNotList, value: matrix[actionNotList][columnSelected] })
+    ]
+  }
+
+  getChallengerPayoff() {
+    const columnWin = 'win'
+    const columnLose = 'lose'
+
+    const candidate = this.getPlayers()[this.candidate]
+
+    const columnSelected = getVerdict(this.getPlayers(), this.voteQuorum) ? columnLose : columnWin
+
+    let matrix = {}
+    matrix[actionChallenge] = {}
+    matrix[actionNotChallenge] = {}
+
+    matrix[actionChallenge][columnWin] = this.minDeposit * this.dispensationPct
+    matrix[actionChallenge][columnLose] = -1 * this.minDeposit
+
+    matrix[actionNotChallenge][columnWin] = 0
+    matrix[actionNotChallenge][columnLose] = 0
+
+    return [
+      new Payoff({ action: actionChallenge, value: matrix[actionChallenge][columnSelected] }),
+      new Payoff({ action: actionNotChallenge, value: matrix[actionNotChallenge][columnSelected] })
+    ]
+  }
+
+  getVoterPayoff(voterIndex) {
+    const columnAccept = 'accept'
+    const columnReject = 'reject'
+
+    const voter = this.getPlayers()[voterIndex]
+    const otherVoters = this.getPlayers().filter((player, index) => index !== voterIndex)
+
+    // hypothetical voting choices
+    const withMeAccepting = otherVoters.concat(new Player({ tokens: voter.tokens, vote: voteAccept }))
+    const withMeRejecting = otherVoters.concat(new Player({ tokens: voter.tokens, vote: voteReject }))
+
+    // tokens excluding voter
+    const acceptBlocTokens = getAcceptBlocTokens(otherVoters)
+    const rejectBlocTokens = getRejectBlocTokens(otherVoters)
+
+    // the outcome of the vote depends on my action (possibly)
+    // therefore the column selected now depends on my action
+    const columnIfIAccept = getVerdict(withMeAccepting, this.voteQuorum) ? columnAccept : columnReject
+    const columnIfIReject = getVerdict(withMeRejecting, this.voteQuorum) ? columnAccept : columnReject
+
+    // percentage including voter
+    const percentOfAcceptBloc = voter.tokens / (acceptBlocTokens + voter.tokens)
+    const percentOfRejectBloc = voter.tokens / (rejectBlocTokens + voter.tokens)
+
+    let matrix = {}
+    matrix[actionAccept] = {}
+    matrix[actionReject] = {}
+
+    matrix[actionAccept][columnAccept] = (1 - this.dispensationPct) * this.minDeposit + rejectBlocTokens * this.minorityBlocSlash * percentOfAcceptBloc
+    matrix[actionAccept][columnReject] = -1 * voter.tokens * this.minorityBlocSlash
+
+    matrix[actionReject][columnAccept] = -1 * voter.tokens * this.minorityBlocSlash
+    matrix[actionReject][columnReject] = (1 - this.dispensationPct) * this.minDeposit + acceptBlocTokens * this.minorityBlocSlash * percentOfRejectBloc
+
+    return [
+      new Payoff({ action: actionAccept, value: matrix[actionAccept][columnIfIAccept] }),
+      new Payoff({ action: actionReject, value: matrix[actionReject][columnIfIReject] })
+    ]
+  }
+
+  getIsChallenged() {
+    return typeof this.challenger === 'number'
+  }
+
+  tokensFor(index) {
+    const all = this.players[index].tokens
+    const hasLocked = (index === this.candidate) || (index === this.challenger) || this.registry.includes(index)
+    const available = hasLocked ? all - this.minDeposit : all
+    return available >= 0 ? available : 0
+  }
+
+  getPlayers() {
+    return this.players.map((player, index) => {
+      let copy = new Player(player)
+      copy.tokens = this.tokensFor(index)
+      return copy
     })
   }
 }
 
-function candidatePayoff(candidate, tcr, columnSelected) {
-  const actionList = 'list'
-  const actionNotList = 'not_list'
-
-  const columnAccept = 'accept'
-  const columnReject = 'reject'
-  const columnNotChallenge = 'not_challenge'
-
-  let matrix = {}
-  matrix[actionList] = {}
-  matrix[actionNotList] = {}
-
-  matrix[actionList][columnAccept] = tcr.minDeposit * tcr.dispensationPct
-  matrix[actionList][columnReject] = -1 * tcr.minDeposit
-  matrix[actionList][columnNotChallenge] = 0
-
-  matrix[actionNotList][columnAccept] = 0
-  matrix[actionNotList][columnReject] = 0
-  matrix[actionNotList][columnNotChallenge] = 0
-
-  return [
-    new Action({ action: actionList, payoff: matrix[actionList][columnSelected] }),
-    new Action({ action: actionNotList, payoff: matrix[actionNotList][columnSelected] })
-  ]
+function getAcceptBlocTokens(voters) {
+  let tokens = voters.filter(({ vote }) => {
+    return vote === voteAccept
+  }).map(({ tokens }) => tokens)
+  return tokens.reduce((total, current) => {
+    return total + current
+  }, 0)
 }
 
-function challengerPayoff(candidate, tcr, columnSelected) {
-  const actionChallenge = 'challenge'
-  const actionNotChallenge = 'not_challenge'
+function getRejectBlocTokens(voters) {
+  return voters.filter(({ vote }) => {
+    return vote === voteReject
+  }).map(({ tokens }) => tokens).reduce((total, current) => {
+    return total + current
+  }, 0)
+}
 
-  const columnWin = 'win'
-  const columnLose = 'lose'
-
-  let matrix = {}
-  matrix[actionChallenge] = {}
-  matrix[actionNotChallenge] = {}
-
-  matrix[actionChallenge][columnWin] = tcr.minDeposit * tcr.dispensationPct
-  matrix[actionChallenge][columnLose] = -1 * tcr.minDeposit
-
-  matrix[actionNotChallenge][columnWin] = 0
-  matrix[actionNotChallenge][columnLose] = 0
-
-  return [
-    new Action({ action: actionChallenge, payoff: matrix[actionChallenge][columnSelected] }),
-    new Action({ action: actionNotChallenge, payoff: matrix[actionNotChallenge][columnSelected] })
-  ]
+function getVerdict(voters, voteQuorum) {
+  const acceptBlocTokens = getAcceptBlocTokens(voters)
+  const rejectBlocTokens = getRejectBlocTokens(voters)
+  return acceptBlocTokens / (acceptBlocTokens + rejectBlocTokens) >= voteQuorum
 }
 
 module.exports = {
+  voteAccept,
+  voteReject,
   Player,
-  TCR,
-  candidatePayoff,
-  challengerPayoff
+  TCR
 }
